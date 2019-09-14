fed9073502c82f62f4663965efbf5d5e54de435e
Lcfvs <lcf.vs@hotmail.com>
2019-09-14 03:01:25 +0200

    + optional node/element.create() ns attribute

--- node/element.js
+++ node/element.js
@@ -3,7 +3,12 @@ import Document from '../prototypes/Document'
 import document from './document'
 
 const method = Document.createElement
+const methodNS = Document.createElementNS
 
-export default function element (tag) {
-  return call(method, document(), tag)
+export default function element (tag, ns = null) {
+  if (ns === null) {
+    return call(method, document(), tag)
+  }
+
+  return call(methodNS, ns, document(), tag)
 }
